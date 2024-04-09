12c7ff0b59b55c70295ec845e4d1fba1210ceb53
saurabhcs1 <singhsaurabhkumar84@gmail.com>
2024-04-09 22:33:41 +0530

    completed without history store

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ function Square({ value, handleClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setxIsNext] = useState(true);
+  let gameEnd = false;
 
   function squareClick(i) {
-    if (squares[i]) {
+    if (squares[i] || gameEnd) {
       return;
     }
     const nextSquare = squares.slice();
@@ -34,8 +35,18 @@ export default function Board() {
     console.log("sqqqq", squares);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    gameEnd = true;
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Chance: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} handleClick={() => squareClick(0)} />
         <Square value={squares[1]} handleClick={() => squareClick(1)} />
@@ -60,3 +71,23 @@ export default function Board() {
 // Lifting state into a parent component is common when React components are refactored.
 
 // setters of useState are async - you won't get updated value just after setValue
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
